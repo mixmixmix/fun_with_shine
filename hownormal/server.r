fb040653fd4c552303069ce99aaec5c6ebfec5ac
Mixmixmix <mikolaj.kundegorski@gmail.com>
2019-04-18 09:53:43 +0100

    Taking input and running a function

--- hownormal/server.r
+++ hownormal/server.r
@@ -1,6 +1,13 @@
 library("shiny")
 
-function(input, output) {
-    output$myFunnyVariabletine = renderText("Hello darkness my old friend, I've come to talk with you again.")
+## displaySum now takes arguments from input so
+## it needs to be on the server side
+displaySum <- function(numberOne, numberTwo){
+    resultOfSum <- numberOne + numberTwo
+    numberAsText <- paste(resultOfSum)
+    return(numberAsText)
+}
 
+function(input, output) {
+    output$myFunnyVariabletine = renderText(displaySum(input$funNumberOne, input$funNumberTwo))
 }
--- hownormal/ui.r
+++ hownormal/ui.r
@@ -1,22 +1,20 @@
 library("shiny")
 
-displaySum <- function(numberOne, numberTwo){
-    resultOfSum <- numberOne + numberTwo
-    numberAsText <- paste(resultOfSum)
-    return(numberAsText)
-}
-
 fluidPage(
     titlePanel("Showing off normal distributions."),
     sidebarLayout(
         sidebarPanel("This is our coat of arms: ",
-                     p(img(src = "https://upload.wikimedia.org/wikipedia/en/0/0b/Uni_Glasgow_2017_arms.png", height = 140))
+                     p(img(src = "https://upload.wikimedia.org/wikipedia/en/0/0b/Uni_Glasgow_2017_arms.png", height = 140)),
+                     numericInput("funNumberOne",
+                                  h3("Fun number:"),
+                                  value = 123),
+                     numericInput("funNumberTwo",
+                                  h3("Another fun number"),
+                                  value = 321)
                      ),
-
         mainPanel(
-            textOutput("myFunnyVariabletine"),
-            h1("Let us calculate 3 + 4:"),
-            h2(displaySum(3,4))
+            h1("Fun sum!"),
+            textOutput("myFunnyVariabletine")
         )
     )
 )
