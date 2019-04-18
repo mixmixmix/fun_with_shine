992d29946f086226b6f52c15765aabf777a6b8a8
Mixmixmix <mikolaj.kundegorski@gmail.com>
2019-04-18 11:00:30 +0100

    Adding a table and a plot

--- hownormal/server.r
+++ hownormal/server.r
@@ -2,12 +2,33 @@ library("shiny")
 
 ## displaySum now takes arguments from input so
 ## it needs to be on the server side
+## I keep all my functions, even if they are not useful.
+## Maybe in the future?
 displaySum <- function(numberOne, numberTwo){
     resultOfSum <- numberOne + numberTwo
     numberAsText <- paste(resultOfSum)
     return(numberAsText)
 }
 
+generateFunnyNormal <- function(meMean, meVariance){
+    n <- 1000
+    meNormal <- rnorm(n,meMean,meVariance)
+    cat(file=stderr(), "Preparing normal with mean ", meMean, "\n")
+    return(meNormal)
+}
+
 function(input, output) {
-    output$myFunnyVariabletine = renderText(displaySum(input$funNumberOne, input$funNumberTwo))
+    output$myFunnyVar <- renderText({
+        paste(input$meanVariance)
+    })
+    output$myFunnyPlot <- renderPlot({
+        hist(generateFunnyNormal(input$meanMean,as.numeric(input$meanVariance)))
+    })
+    output$myFunnyTable <- renderTable({
+        table(
+            head(
+                generateFunnyNormal(input$meanMean,as.numeric(input$meanVariance))
+            )
+        )
+    })
 }
--- hownormal/ui.r
+++ hownormal/ui.r
@@ -5,16 +5,23 @@ fluidPage(
     sidebarLayout(
         sidebarPanel("This is our coat of arms: ",
                      p(img(src = "https://upload.wikimedia.org/wikipedia/en/0/0b/Uni_Glasgow_2017_arms.png", height = 140)),
-                     numericInput("funNumberOne",
-                                  h3("Fun number:"),
-                                  value = 123),
-                     numericInput("funNumberTwo",
-                                  h3("Another fun number"),
-                                  value = 321)
+                     selectInput("meanVariance",
+                                  h3("unfun Variances to choose from:"),
+                                  choices = list(
+                                      "small" = 1,
+                                      "so, so" = 10,
+                                      "a big one!" = 33),
+                                  selected = 10),
+                     sliderInput("meanMean",
+                                  h3("Very mean, mean :/"),
+                                  min = 0,
+                                  max = 100,
+                                  value = 17)
                      ),
         mainPanel(
             h1("Fun sum!"),
-            textOutput("myFunnyVariabletine")
+            plotOutput(outputId = "myFunnyPlot"),
+            tableOutput("myFunnyTable")
         )
     )
 )
